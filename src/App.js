60f36e342fc017d54255f870850a478f57ed8e0c
krtu0p <109890115+krtu0p@users.noreply.github.com>
2024-10-31 21:33:54 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,20 +8,19 @@ function App() {
   const [articleContent, setArticleContent] = useState('');
   const [searchAttempted, setSearchAttempted] = useState(false);
 
+  // Use the environment variable for the API URL
+  const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
-  // Function to handle the search operation
   const handleSearch = async (e) => {
     e.preventDefault();
     setSearchAttempted(true);
     console.log('Searching for:', searchTerm);
-  
     try {
-      const response = await axios.get(`http://localhost:8000/search?q=${searchTerm}`);
+      const response = await axios.get(`${apiUrl}/search?q=${searchTerm}`);
       console.log('Full response:', response);
-  
-      // Adjust this line to access the new structure
-      if (response.data && response.data.query && response.data.query.search) {
-        setResults(response.data.query.search);
+      console.log('Response data:', response.data);
+      if (response.data && response.data.search) {
+        setResults(response.data.search);
       } else {
         console.error('Unexpected response data structure:', response.data);
         setResults([]);
@@ -35,32 +34,28 @@ function App() {
       alert('An error occurred while fetching search results. Please try again later.');
     }
   };
-  
-  // Function to handle clicking on a search result
+
   const handleResultClick = async (pageId) => {
     try {
       const response = await axios.get(`https://en.wikipedia.org/w/api.php?action=parse&pageid=${pageId}&format=json&origin=*`);
-      console.log('Article content response:', response);
       setArticleContent(response.data.parse.text['*']);
     } catch (error) {
       console.error('Error fetching article content:', error);
-      alert('An error occurred while fetching the article content. Please try again later.');
     }
   };
 
-  // Render the search results
   const renderResults = () => {
     if (searchAttempted && results.length > 0) {
       return results.map((result) => (
         <div key={result.pageid} onClick={() => handleResultClick(result.pageid)} className="result-item">
           <h3>{result.title}</h3>
-          <p dangerouslySetInnerHTML={{ __html: result.snippet }} />
+          <p>{result.snippet}</p>
         </div>
       ));
     } else if (searchAttempted) {
       return <p>No results found.</p>;
     } else {
-      return <p>Please enter a search term to get started.</p>;
+      return null;
     }
   };
 
@@ -76,11 +71,11 @@ function App() {
         />
         <button type="submit">Search</button>
       </form>
-
+  
       <div className="results-container">
         {renderResults()}
       </div>
-
+  
       {articleContent && (
         <div className="article-container">
           <h2 className="article-title">Article Details</h2>
